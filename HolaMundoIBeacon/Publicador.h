65ddcf423941e063ca59d5b6d6f9ec23db6dd52c
Rekken4848 <hugomarescrihuela@gmail.com>
2023-10-16 07:54:05 +0200

    publicacionFinalCreada

--- HolaMundoIBeacon/Publicador.h
+++ HolaMundoIBeacon/Publicador.h
@@ -8,6 +8,27 @@
 #ifndef PUBLICADOR_H_INCLUIDO
 #define PUBLICADOR_H_INCLUIDO
 
+// ............................................................
+//   obtenerDispositivoID() --> Texto
+// ............................................................
+char[] obtenerDispositivoID(){
+  // Obtiene el identificador único del chip NRF52840
+  uint32_t uniqueID[3];
+  NRF_FICR->DEVICEID[0] = NRF_FICR->DEVICEID[0] & 0xFFFFFFFC; // Garantiza que el ID sea único
+  uniqueID[0] = NRF_FICR->DEVICEID[0];
+  uniqueID[1] = NRF_FICR->DEVICEID[1];
+  uniqueID[2] = NRF_FICR->DEVICEID[2];
+  
+  // Envía el identificador único por Bluetooth
+  char uniqueIDString[32];
+  snprintf(uniqueIDString, sizeof(uniqueIDString), "%08X%08X%08X", uniqueID[0], uniqueID[1], uniqueID[2]);
+
+  elPuerto.escribir( "\n---- id prueboID(): empieza " );
+  elPuerto.escribir( uniqueIDString );
+  elPuerto.escribir( "\n---- id prueboID(): acaba \n" );
+
+  return uniqueIDString;
+}
 // --------------------------------------------------------------
 // --------------------------------------------------------------
 class Publicador {
@@ -27,7 +48,8 @@ public:
   EmisoraBLE laEmisora {
 	"GTI-3A", //  nombre emisora
 	  0x004c, // fabricanteID (Apple)
-	  4 // txPower
+	  //4 // txPower
+    obtenerDispositivoID()
 	  };
   
   const int RSSI = -53; // por poner algo, de momento no lo uso
@@ -60,7 +82,7 @@ public:
   } // ()
 
   // ............................................................
-  //   publicarCO2() --> N, N, N
+  //   publicarCO2() <-- N, N, N
   // ............................................................
   void publicarCO2( int16_t valorCO2, uint8_t contador,
 					long tiempoEspera ) {
@@ -97,7 +119,7 @@ public:
   } // ()
 
   // ............................................................
-  //   publicarTemperatura() --> N, N, N
+  //   publicarTemperatura() <-- N, N, N
   // ............................................................
   void publicarTemperatura( int16_t valorTemperatura,
 							uint8_t contador, long tiempoEspera ) {
@@ -105,6 +127,21 @@ public:
 	uint16_t major = (MedicionesID::TEMPERATURA << 8) + contador;
 	(*this).laEmisora.emitirAnuncioIBeacon( (*this).beaconUUID, 
 											major,
+											valorTemperatura, // minor
+											(*this).RSSI // rssi
+									);
+	esperar( tiempoEspera );
+
+	(*this).laEmisora.detenerAnuncio();
+  } // ()
+
+  // ............................................................
+  //   publicarTemperaturaGasId() <-- R, R
+  // ............................................................
+  void publicarTemperaturaGasId( double valorTemperatura,
+							double valorGas ) {
+	(*this).laEmisora.emitirAnuncioIBeacon( (*this).beaconUUID, 
+											valorGas,
 											valorTemperatura, // minor
 											(*this).RSSI // rssi
 									);
